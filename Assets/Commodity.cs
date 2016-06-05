443845b25c8c507a74a4af31e730a4d6dd513659
免费打工仔 <nethz@163.com>
2016-06-06 00:11:34 +0800

    shop
    ....

---
+++ Assets/Commodity.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using GDGeek;
+public class Commodity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	Vector3 toScale(float x){
+		float r = (x + 7.0f) / 14.0f;
+		float c = Tween.easeInOutSine (0.0f, 1.0f, r*2.0f)+1.0f;
+		return new Vector3(c,c,c);
+	}
+	// Update is called once per frame
+	void Update () {
+		if (this.transform.position.x > -7.0f && this.transform.position.x < 7.0f) {
+			this.transform.localScale = toScale (this.transform.position.x);
+		} else {
+			this.transform.localScale = Vector3.one;
+		}
+	}
+}
---
+++ Assets/Shop.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using GDGeek;
+
+public class Shop : MonoBehaviour {
+	private Commodity[] commodities_ = null;
+	public GameObject _offset = null;
+	private float offset_ = 0.0f;
+	private FSM fsm_ = new FSM();
+	public int _select = 0;
+	private const float Jianju_ = 15.0f;
+	private State getNormal(){
+		StateWithEventMap normal = new StateWithEventMap (); 	
+		normal.addAction ("swipe_start", "touch");
+		normal.onStart += delegate {
+	//		Debug.Log("normal");
+		};
+		return normal;
+	}
+	private State getTouch(){
+		StateWithEventMap touch = new StateWithEventMap (); 	
+		touch.addAction ("swipe_end", "select");
+		touch.onStart += delegate {
+			
+		//	Debug.Log("touch");
+		};
+		touch.onOver += delegate {
+			_select = -(int)(_offset.transform.position.x/Jianju_ -0.5f) ;
+			Debug.Log(_select);
+		};
+
+		return touch;
+	}
+	private State getSelect(){
+		StateWithEventMap select = TaskState.Create (delegate {
+			TweenTask tt = new TweenTask(delegate() {
+				TweenLocalPosition tlp = TweenLocalPosition.Begin(this._offset, 0.3f, new Vector3(-_select * Jianju_, 0, 0) );
+
+				return tlp;
+			});
+			//TweenLocalPosition();
+			/*
+			Task task = new Task();
+			task.init = delegate {
+				Debug.Log(_select);	
+			};
+			return task;*/
+			return tt;
+		}, fsm_, "normal");	
+
+		select.onStart += delegate {
+//			Debug.Log("select");
+		};
+		return select;
+	}
+	void Awake(){
+		fsm_.addState ("normal", getNormal());
+		fsm_.addState ("touch", getTouch());
+		fsm_.addState ("select", getSelect ());
+		fsm_.init ("normal");
+	
+	}
+	private void layout(){
+		for (int i = 0; i < commodities_.Length; ++i) {
+			var pos = new Vector3 (i*Jianju_, 0, 0);
+			commodities_ [i].transform.localPosition = pos;
+		}
+		
+	}
+	private Vector3 handlePosition(Vector2 delta){
+		offset_ += delta.x;
+		return new Vector3(offset_, 0,0);
+	}
+	// Use this for initialization
+	void Start () {
+		commodities_ = this.gameObject.GetComponentsInChildren<Commodity> ();
+		//Debug.Log (commodities_.Length);
+		this.layout ();
+		EasyTouch.On_SwipeStart += delegate(Gesture gesture) {
+			fsm_.post("swipe_start");
+		};
+		EasyTouch.On_SwipeEnd += delegate(Gesture gesture) {
+			fsm_.post("swipe_end");
+		};
+		EasyTouch.On_Swipe += delegate(Gesture gesture) {
+			_offset.transform.localPosition = handlePosition(gesture.deltaPosition);
+
+		};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
